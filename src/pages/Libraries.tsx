70c33378955cb8a3012b6067faa7cdee74e4b55e
Alibek Tauirbaev <featternotving@gmail.com>
2025-07-03 21:23:12 +0500

    Add libraries table

--- src/pages/Libraries.tsx
+++ src/pages/Libraries.tsx
@@ -1,9 +1,33 @@
 import useLibraries from "../hooks/use-libraries.ts";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
 
 const Libraries = () => {
   const { data } = useLibraries();
 
-  return <div>Libraries</div>;
+  return (
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Latest</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data?.results.map((library) => (
+          <TableRow key={library.latest}>
+            <TableCell>{library.name}</TableCell>
+            <TableCell>{library.latest}</TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 };
 
 export default Libraries;
